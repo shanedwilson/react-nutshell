8d207785689d072856eacf4232d3a7f6fd32dcf7
Shane D Wilson <me@shanedwilson.com>
2019-01-12 11:18:26 -0600

    getting data from firebase

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -13,7 +13,7 @@ class Home extends React.Component {
         <div className="card-deck mt-5">
           <div className="card border-dark" id="messages" onClick={this.changeView}>
             <div className="card-body text-center">
-              <h4 class="card-title"><i className="fas fa-comments fa-7x"></i></h4>
+              <h4 className="card-title"><i className="fas fa-comments fa-7x"></i></h4>
               <h6 className="card-subtitle mb-2 text-muted">Messages</h6>
               <p className="card-text">Newer better AOL</p>
             </div>
--- src/components/pages/Messages/Messages.js
+++ src/components/pages/Messages/Messages.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import messageRequests from '../../../helpers/data/messageRequests';
 import './Messages.scss';
 
 class Messages extends React.Component {
+  state = {
+    messages: [],
+  }
+
+  componentDidMount() {
+    messageRequests.getAllMessages()
+      .then((messages) => {
+        this.setState({ messages });
+        console.log(messages);
+      })
+      .catch((err) => {
+        console.error('error with friends GET', err);
+      });
+  }
+
   render() {
     return (
       <div className="messages mx-auto">
