691575a29c0c0f31764bbc9f25f13eca9006e276
Shane D Wilson <me@shanedwilson.com>
2019-01-12 13:40:54 -0600

    delete working

--- src/components/SingleMessage/SingleMessage.js
+++ src/components/SingleMessage/SingleMessage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import authRequests from '../../helpers/data/authRequests';
 import messageShape from '../../helpers/propz/messageShape';
 
 import './SingleMessage.scss';
@@ -6,16 +8,39 @@ import './SingleMessage.scss';
 class SingleMessage extends React.Component {
   static propTypes = {
     message: messageShape,
+    deleteSingleMessage: PropTypes.func,
   }
 
-  render() {
-    return (
-      <div className="single-message mt-3">
-      <span className="message-user col-3">{this.props.message.userName}</span>
-      <span className="message-text col-7 justify-content-end">{this.props.message.message}</span>
-      </div>
-    );
-  }
+    deleteEvent = (e) => {
+      e.preventDefault();
+      const { deleteSingleMessage, message } = this.props;
+      deleteSingleMessage(message.id);
+    }
+
+    render() {
+      const uid = authRequests.getCurrentUid();
+
+      const makeButtons = () => {
+        if (this.props.message.uid === uid) {
+          return (
+          <div>
+              <button className="btn btn-default" onClick={this.deleteEvent}>
+                <i className="fas fa-trash-alt"></i>
+              </button>
+          </div>
+          );
+        }
+        return <span className="col-2"></span>;
+      };
+
+      return (
+        <div className="single-message mt-3">
+        <span className="message-user col-3">{this.props.message.userName}</span>
+        <span className="message-text col-7 justify-content-end">{this.props.message.message}</span>
+        <span className="col-1">{makeButtons()}</span>
+        </div>
+      );
+    }
 }
 
 export default SingleMessage;
--- src/components/pages/Messages/Messages.js
+++ src/components/pages/Messages/Messages.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import SingleMessage from '../../SingleMessage/SingleMessage';
-import './Messages.scss';
 import smashRequests from '../../../helpers/data/smashRequests';
+import messageRequests from '../../../helpers/data/messageRequests';
+
+import './Messages.scss';
 
 class Messages extends React.Component {
   state = {
@@ -11,7 +13,7 @@ class Messages extends React.Component {
   componentDidMount() {
     smashRequests.getAllMessagesWithUserInfo()
       .then((messages) => {
-        if (messages.length > 1) {
+        if (messages.length > 10) {
           messages.shift(messages.length - 1, messages.length);
         }
         this.setState({ messages });
@@ -21,22 +23,34 @@ class Messages extends React.Component {
       });
   }
 
-  render() {
-    const messageItems = this.state.messages.map(message => (
+    deleteSingleMessage = (messageId) => {
+      messageRequests.deleteMessage(messageId)
+        .then(() => {
+          smashRequests.getAllMessagesWithUserInfo()
+            .then((messages) => {
+              this.setState({ messages });
+            });
+        })
+        .catch(err => console.error('error with delete single', err));
+    }
+
+    render() {
+      const messageItems = this.state.messages.map(message => (
     <SingleMessage
     key={message.id}
     message={message}
+    deleteSingleMessage={this.deleteSingleMessage}
     />
-    ));
-    return (
+      ));
+      return (
       <div className="messages-container mx-auto">
         <h2>Messages</h2>
         <div className="messages">
           <div className="">{messageItems}</div>
         </div>
       </div>
-    );
-  }
+      );
+    }
 }
 
 export default Messages;
